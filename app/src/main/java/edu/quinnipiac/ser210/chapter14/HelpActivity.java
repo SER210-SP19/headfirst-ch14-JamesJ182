70d730ce9c8ea180e426d5329bb68c98947068e6
JamesJ182 <jjacobson@quinnipiac.edu>
2019-04-16 01:30:28 -0400

    First Commit

---
+++ app/src/main/java/edu/quinnipiac/ser210/chapter14/HelpActivity.java
@@ -0,0 +1,17 @@
+package edu.quinnipiac.ser210.chapter14;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
